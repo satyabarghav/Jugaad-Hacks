8fba0a756a048d7c082847cecdad67f0d1579c32
Appari Satya Barghav <36763910+satyabarghav@users.noreply.github.com>
2024-06-02 20:19:30 +0530

    Using the Flask g object for more flexibility

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 from llm_main import generate_messages
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-email_content = ""
+app.config['EMAIL_CONTENT'] = ""
 
 @app.route("/")
 def index():
@@ -13,22 +13,18 @@ def index():
 
 @app.route("/setContent", methods=["POST"])
 def set_content():
-    global email_content
-
     data = request.get_json()
     if "content" in data:
-        email_content = data["content"]
+        app.config['EMAIL_CONTENT'] = data["content"]
         return jsonify({"message": "Email content updated successfully"}), 200
     else:
         return jsonify({"error": "Content not provided in the request"}), 400
 
 @app.route("/generate", methods=["GET"])
 def generate():
-    global email_content
-
+    email_content = app.config.get('EMAIL_CONTENT', '')
     if email_content:
         response = generate_messages(email_content)
-        email_content = ""
         return jsonify({"message": response})
     else:
         return jsonify({"error": "No data available"}), 404
